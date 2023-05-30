f33d0943845f5faed255c734f2fa982e53fc2018
kongkham-lk <kongkham.luangkhot@gmail.com>
2023-05-30 03:24:51 -0700

    added course work files

---
+++ YelpCamp/v6 - adding authetication/models/review.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema({
+    body: String,
+    rating: Number,
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+})
+
+// to connect review model with campground model => update campground schema's structure by adding reviews field and review's objectID as value
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
